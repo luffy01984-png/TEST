75e5642f5756b7ac0555199d54a2db2a474c7fec
luffy01984-png <luffy01984@gmail.com>
2025-11-21 22:26:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'renault-trucks-cse-v1.8'; // Incrémentez à chaque update
+const CACHE_NAME = 'renault-trucks-cse-v1.9'; // Incrémentez à chaque update
 
 // Ressources à pré-cacher (offline)
 const PRECACHE = [
